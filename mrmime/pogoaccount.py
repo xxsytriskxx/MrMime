2769e5763a036623eb3b5964d79aeb61f972c23f
SkOODaT <skoodat@live.com>
2017-11-16 23:34:35 -0500

    Update device_info devices and iOS versions
    Update device_info devices and iOS versions.

--- mrmime/pogoaccount.py
+++ mrmime/pogoaccount.py
@@ -564,14 +564,19 @@ class POGOAccount(object):
             'iPhone9,1': 'D10AP',
             'iPhone9,2': 'D11AP',
             'iPhone9,3': 'D101AP',
-            'iPhone9,4': 'D111AP'
+            'iPhone9,4': 'D111AP',
+            'iPhone10,1': 'D20AP',
+            'iPhone10,2': 'D21AP',
+            'iPhone10,3': 'D22AP',
+            'iPhone10,4': 'D201AP',
+            'iPhone10,5': 'D211AP',
+            'iPhone10,6': 'D221AP'
         }
 
-        ios8 = ('8.0', '8.0.1', '8.0.2', '8.1', '8.1.1',
-                '8.1.2', '8.1.3', '8.2', '8.3', '8.4', '8.4.1')
         ios9 = ('9.0', '9.0.1', '9.0.2', '9.1', '9.2', '9.2.1',
                 '9.3', '9.3.1', '9.3.2', '9.3.3', '9.3.4', '9.3.5')
         ios10 = ('10.0', '10.0.1', '10.0.2', '10.0.3', '10.1', '10.1.1')
+        ios11 = ('11.0.1', '11.0.2', '11.0.3', '11.1', '11.1.1')
 
         device_info = {
             'device_brand': 'Apple',
@@ -586,12 +591,14 @@ class POGOAccount(object):
         device_info['hardware_model'] = iphones[device]
         device_info['device_id'] = md5.hexdigest()
 
-        if device.startswith('iPhone9'):
-            ios_pool = ios10
+        if device.startswith('iPhone10'):
+            ios_pool = ios11
+        elif device.startswith('iPhone9'):
+            ios_pool = ios10 + ios11
         elif device.startswith('iPhone8'):
-            ios_pool = ios9 + ios10
+            ios_pool = ios9 + ios10 + ios11
         else:
-            ios_pool = ios8 + ios9 + ios10
+            ios_pool = ios9 + ios10
         device_info['firmware_type'] = ios_pool[pick_hash % len(ios_pool)]
 
         self.log_debug("Using an {} on iOS {} with device ID {}".format(device,
