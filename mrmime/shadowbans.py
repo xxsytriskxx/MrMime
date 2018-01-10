ec65b36fb661221f456a564a027eff7b6c43f8c5
sLoPPydrive <sloppydrive@gmail.com>
2018-01-10 09:35:24 +0100

    New list of common Pokemon for shadowban detection
    Source: https://github.com/Hydro74000/Monocle/commit/a9a1c45dadb4833930856df7137bd09e016f4690

--- mrmime/shadowbans.py
+++ mrmime/shadowbans.py
@@ -7,32 +7,28 @@ COMMON_POKEMON = [
     27,     # Sandshrew
     29,     # Nidoran F
     32,     # Nidoran M
-    41,     # Zubat
     43,     # Oddish
     46,     # Paras
     52,     # Meowth
     54,     # Psyduck
     60,     # Poliwag
     69,     # Bellsprout
-    72,     # Tentacool
-    74,     # Geodude
     77,     # Ponyta
     81,     # Magnemite
     98,     # Krabby
     118,    # Goldeen
     120,    # Staryu
     129,    # Magikarp
-    161,    # Sentret
-    165,    # Ledyba
-    167,    # Spinarak
     177,    # Natu
     183,    # Marill
     187,    # Hoppip
     191,    # Sunkern
     194,    # Wooper
-    198,    # Murkrow
     209,    # Snubbull
-    218     # Slugma
+    218,    # Slugma
+    320,    # Wailmer
+    325,    # Spoink
+    339     # Barboach
 ]
 
 
