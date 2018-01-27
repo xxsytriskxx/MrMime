6f02a4ef25049e3ea918772f5e44389789856569
xxsytriskxx <Azure.flame@live.com>
2018-01-27 15:41:18 +0400

    Update shadowbans.py

--- mrmime/shadowbans.py
+++ mrmime/shadowbans.py
@@ -26,6 +26,8 @@ COMMON_POKEMON = [
     194,    # Wooper
     209,    # Snubbull
     218,    # Slugma
+    293,    # Whismur
+    304,    # Aron
     320,    # Wailmer
     325,    # Spoink
     339     # Barboach
